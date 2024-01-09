8607f893fe8f4fe209d98c6815963b8c87180fb6
Mara Caoile <mara.caoile@gmail.com>
2024-01-08 19:04:09 -0600

    updated urls.py for auth

--- bangazonapi/views/user_auth.py
+++ bangazonapi/views/user_auth.py
@@ -1,7 +1,5 @@
 from rest_framework.decorators import api_view
-from django.http import HttpResponseServerError
-from rest_framework.decorators import action
-from rest_framework.decorators import Response
+from rest_framework.response import Response
 from bangazonapi.models import AdminUser
 
 @api_view(['POST'])
