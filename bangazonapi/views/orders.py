cef761e860b04c0304ffb78120e650c589c5ba71
Mara Caoile <mara.caoile@gmail.com>
2024-01-11 18:19:49 -0600

    refactored get all orders to be UID authorized

--- bangazonapi/views/orders.py
+++ bangazonapi/views/orders.py
@@ -24,7 +24,10 @@ class OrderView(ViewSet):
     """Handles GET request for all orders"""
     
     order = Order.objects.all()
-    serializer = OrderSerializer(order, many=True)
+    admin_user = AdminUser.objects.get(uid=request.META['HTTP_AUTHORIZATION'])
+    associated_user = order.filter(admin_user=admin_user)
+  
+    serializer = OrderSerializer(associated_user, many=True)
     return Response(serializer.data)
   
   @action(methods=['get'], detail=True)
@@ -36,34 +39,34 @@ class OrderView(ViewSet):
     serializer = OrderItemSerializer(associated_order, many=True)
     return Response(serializer.data)
   
-  @action(methods=['delete'], detail=True)
-  def remove(self, request, pk):
-    """Method to delete items associted to a single order"""
-    admin_user = AdminUser.objects.get(uid=request.META['HTTP_AUTHORIZATION'])
-    orders = Order.objects.all()
-    associated_user_order = orders.filter(admin_user=admin_user)
+  # @action(methods=['delete'], detail=True)
+  # def remove(self, request, pk):
+  #   """Method to delete items associted to a single order"""
+  #   admin_user = AdminUser.objects.get(uid=request.META['HTTP_AUTHORIZATION'])
+  #   orders = Order.objects.all()
+  #   associated_user_order = orders.filter(admin_user=admin_user)
     
-    item = MenuItem.objects.get(pk=pk)
+  #   item = MenuItem.objects.get(pk=pk)
     
-    order_item = OrderItem.objects.get(
-      order = associated_user_order,
-      menu_item = item
-    )
-    order_item.delete()
-    return Response({'message': 'Item removed'}, status=status.HTTP_204_NO_CONTENT)
+  #   order_item = OrderItem.objects.get(
+  #     order = associated_user_order,
+  #     menu_item = item
+  #   )
+  #   order_item.delete()
+  #   return Response({'message': 'Item removed'}, status=status.HTTP_204_NO_CONTENT)
   
-  @action(methods=['post'], detail=True)
-  def add(self, request, pk):
-    """Method to add items associted to a single order"""
-    admin_user = AdminUser.objects.get(uid=request.META['HTTP_AUTHORIZATION'])
-    orders = Order.objects.all()
-    associated_user_order = orders.filter(admin_user=admin_user)
+  # @action(methods=['post'], detail=True)
+  # def add(self, request, pk):
+  #   """Method to add items associted to a single order"""
+  #   admin_user = AdminUser.objects.get(uid=request.META['HTTP_AUTHORIZATION'])
+  #   orders = Order.objects.all()
+  #   associated_user_order = orders.filter(admin_user=admin_user)
     
-    item = MenuItem.objects.get(pk=pk)
+  #   item = MenuItem.objects.get(pk=pk)
     
-    order_item = OrderItem.objects.create(
-      order = associated_user_order,
-      menu_item = item
-    )
-    return Response({'message': 'Item removed'}, status=status.HTTP_204_NO_CONTENT)
+  #   order_item = OrderItem.objects.create(
+  #     order = associated_user_order,
+  #     menu_item = item
+  #   )
+  #   return Response({'message': 'Item removed'}, status=status.HTTP_204_NO_CONTENT)
     
