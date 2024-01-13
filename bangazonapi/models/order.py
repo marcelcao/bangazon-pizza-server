9ce077ea5156a8bb7faf906757ac102d7c254de4
Mara Caoile <mara.caoile@gmail.com>
2024-01-13 14:03:15 -0600

    order total functioning

--- bangazonapi/models/order.py
+++ bangazonapi/models/order.py
@@ -10,3 +10,11 @@ class Order(models.Model):
     customer_email = models.CharField(max_length=50)
     order_type = models.ForeignKey(OrderCategory, on_delete=models.CASCADE)
     is_closed = models.BooleanField()
+    
+    @property
+    def total_order(self):
+        """Sum of menu items"""
+        order_items = getattr(self, 'order_items', None)
+        if order_items is not None:
+            return sum([order_item.menu_item.item_price for order_item in order_items.all()])
+        return 0
--- bangazonapi/models/order_item.py
+++ bangazonapi/models/order_item.py
@@ -4,5 +4,5 @@ from .menu_item import MenuItem
 
 class OrderItem(models.Model):
 
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_items")
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
